598079c9b693f0d39f9350549c5ef1ff8edfb0f5
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-19 15:39:07 +0900

    Update ApiLoginSuccessHandler.java

--- src/main/java/com/example/securityapp/security/handler/ApiLoginSuccessHandler.java
+++ src/main/java/com/example/securityapp/security/handler/ApiLoginSuccessHandler.java
@@ -3,14 +3,11 @@ package com.example.securityapp.security.handler;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
 import com.example.securityapp.dto.MemberDto;
 import com.example.securityapp.util.JWTUtil;
 import com.google.gson.Gson;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
